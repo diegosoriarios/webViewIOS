558fd54c04a9cc765c3b97d3c95d884f93a4aab2
diegosoriarios <diegosoriarios@gmail.com>
2019-05-23 12:58:41 -0300

    Final

--- webviewSwift/FirstViewController.swift
+++ webviewSwift/FirstViewController.swift
@@ -16,7 +16,7 @@ class FirstViewController: UIViewController, WKUIDelegate {
         super.viewDidLoad()
         
         if ReachabilityTest.isConnectedToNetwork() {
-            let myURL = URL(string:"https://admin:admin@ccstore-prod-zdoa.oracleoutsourcing.com")
+            let myURL = URL(string:"https://www.chinainbox.com.br/")
             let myRequest = URLRequest(url: myURL!)
             webView.load(myRequest)
             
